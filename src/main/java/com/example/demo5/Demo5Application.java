fc7137f6c4f18c9175f2641439f965bb899d1ede
Agam Singh <bagoli.efogej@rungel.net>
2024-05-06 17:42:59 +0530

    second commit

--- src/main/java/com/example/demo5/Demo5Application.java
+++ src/main/java/com/example/demo5/Demo5Application.java
@@ -1,12 +1,23 @@
 package com.example.demo5;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class Demo5Application {
-
-	public static void main(String[] args) {
+	  public static Logger logger=LoggerFactory.getLogger(Demo5Application.class);
+	     @PostConstruct
+	     public void intt() {
+	    	 logger.info("Application started......");
+	     }
+		public static void main(String[] args) {
+			logger.info("Application executed.......");
 		SpringApplication.run(Demo5Application.class, args);
 	}
 
--- src/test/java/com/example/demo5/Demo5ApplicationTests.java
+++ src/test/java/com/example/demo5/Demo5ApplicationTests.java
@@ -1,13 +1,20 @@
 package com.example.demo5;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Demo5ApplicationTests {
+	public static Logger logger=LoggerFactory.getLogger(Demo5ApplicationTests.class);
 
 	@Test
 	void contextLoads() {
-	}
+		logger.info("Application executed.......");
+		assertEquals(true, true);
+		}
 
 }
